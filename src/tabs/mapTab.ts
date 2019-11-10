4bf11a72b4b117687ec4f96427995c7253fd187e
fax1ty <fax1ty.ru@gmail.com>
2019-11-10 19:47:17 +0400

    v0.0.5

--- src/tabs/mapTab.ts
+++ src/tabs/mapTab.ts
@@ -144,7 +144,10 @@ export = new Tab({ left: 0, right: 0, top: 0, bottom: 0 })
                                                                                 new Composite({ height: 80, width: 80, background: currentStyle.colors.contrast, cornerRadius: 18, highlightOnTouch: true })
                                                                                     .append(
                                                                                         new ImageView({ centerX: 0, centerY: 0, width: 15, image: currentStyle.icons.plus, tintColor: currentStyle.colors.opposite })
-                                                                                    ),
+                                                                                    )
+                                                                                    .onTap(() => {
+
+                                                                                    }),
                                                                                 new Composite({ left: 'prev() 25', height: 80, width: 80, background: currentStyle.colors.contrast, cornerRadius: 18, highlightOnTouch: true })
                                                                                     .append(
                                                                                         new ImageView({ centerX: 0, centerY: 0, width: 15, image: currentStyle.icons.plus, tintColor: currentStyle.colors.opposite })
